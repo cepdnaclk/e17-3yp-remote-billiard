4050b0e265ab33f363b0228f2c5f3432a810b4fb
unknown <e17327@eng.pdn.ac.lk>
2021-08-30 16:00:34 +0530

    mobile app implementations

---
+++ Remote_billiard_app/flutter app/firstapp/lib/main.dart
@@ -0,0 +1,23 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Remote Billiard',
+      home: HomePage(),
+      
+    );
+  }
+}
+
---
+++ Remote_billiard_app/flutter app/firstapp/lib/text_field_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+  const TextFieldContainer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: Colors.lightBlue[200].withOpacity(0.8),
+        borderRadius: BorderRadius.circular(29),
+
+      ),
+      child: child,
+    );
+  }
+}
