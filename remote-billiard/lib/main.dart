533cba46569ec156b2889962193d1d8620dc3607
unknown <e17327@eng.pdn.ac.lk>
2021-09-26 12:26:09 +0530

    home page and main

--- remote-billiard/lib/main.dart
+++ remote-billiard/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import './ChatPage.dart';
 import './HomePage.dart';
-import'./ExitPage.dart';
-import'./ChoosePocket.dart';
+import './ExitPage.dart';
+import './SelectOptions.dart';
+import './FindPlayer.dart';
 
 void main() => runApp(MyMaterial());
 
@@ -10,16 +11,14 @@ class MyMaterial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute:HomePage.id,
-      routes:{
-        HomePage.id:(context)=>HomePage(),
-         //ChatPage.id:(context)=>ChatPage(),
-         ChoosePocket.id:(context)=>ChoosePocket(),
-       ExitPage.id:(context)=>ExitPage(),
-
-      }
-     
-    );
+        debugShowCheckedModeBanner: false,
+        initialRoute: FindPlayer.id,
+        routes: {
+          HomePage.id: (context) => HomePage(),
+          //ChatPage.id:(context)=>ChatPage(),
+          ExitPage.id: (context) => ExitPage(),
+          SelectOptions.id: (context) => SelectOptions(),
+          FindPlayer.id: (context) => FindPlayer(),
+        });
   }
 }
